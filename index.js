02b050e973643f458708317d466f86de93d295bc
ThongchaiPornchaikit <67070233@kmitl.ac.th>
2026-03-10 01:06:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1914,7 +1914,6 @@ app.listen(port, () => {
     // db.get("PRAGMA foreign_keys = ON;", (err, res) => {
     //     console.log(res);
     // })
-    
     console.log("Server started.");
     
 }); 
